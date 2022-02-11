fda646ac8c69be036882f9e31925a508b4b0f07b
Loïc Rojon <loic.rojon@gmail.com>
2022-02-11 11:35:57 +0100

    Ajout constant pour logique

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const SALARY = document.querySelector('#salary')
+const TAB = document.querySelector('#tab')
+const SAVING = document.querySelector('#saving')
+const PLEASURE = document.querySelector('#pleasure')
